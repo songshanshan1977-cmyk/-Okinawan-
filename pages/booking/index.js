99c363d93130e4e3df00788e46dde14d2bebb990
songshanshan1977-cmyk <songshanshan1977@gmail.com>
2025-12-10 11:07:44 +0900

    Create index.js

---
+++ pages/booking/index.js
@@ -0,0 +1,12 @@
+export default function BookingHome() {
+  return (
+    <div style={{ padding: "40px", fontSize: "22px" }}>
+      <h1>Booking Page</h1>
+      <p>This is the Booking entry page ✔</p>
+
+      <a href="/" style={{ color: "blue" }}>
+        ← Back to Home
+      </a>
+    </div>
+  );
+}
